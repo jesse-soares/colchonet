0a03e3a4af031686db8a0d926b61560b3ce8323e
Jessé Cirilo Soares <jessecsoares@gmail.com>
2014-03-26 19:00:46 -0300

    Criacao do projeto rails

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ colchonet
 =========
 
 Project for learning Ruby on Rails
+
+* Ruby version: ruby 2.0.0p195
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Colchonet::Application.config.secret_key_base = '35510055e8f4bf925b653ea8b264e4b7fc57a63aa4fb50bddf6c8c6263260a083fe8e05f751dfde22f03c1e3798ad114f85da433689f71d9e9e1be751b8c528e'
